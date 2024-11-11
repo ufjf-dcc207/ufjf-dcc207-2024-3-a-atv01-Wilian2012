9bcca75a3929e069bacd0f689e726766ee98e654
wilian santos <14809032647@estudante.ufjf.br>
2024-11-11 20:36:06 -0300

    adiciona stilo

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,11 +1,14 @@
+import "./Disciplina.css";
+
 function Disciplina(){
 
-    return <div>
+    return (
+    <div className="disciplina">
 
             <div>DCC207</div>
             <div>Desenvolvimento Web Front End</div>
     </div>
-   
+    );
 }
 
 export default Disciplina;
